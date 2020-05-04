f0cf0122b01eb6ccdd0e24f3d11c5b8052e33114
Septimiu Bar <bar.septimiu@gmail.com>
2020-05-04 12:02:19 +0300

    First commit

---
+++ LibOverride/GUI/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.password_textBox = new System.Windows.Forms.TextBox();
+            this.port_textBox = new System.Windows.Forms.TextBox();
+            this.ipAddress_textBox = new System.Windows.Forms.TextBox();
+            this.username_textBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SaveLib_button = new System.Windows.Forms.Button();
+            this.Override_button = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.connect_button);
+            this.panel1.Controls.Add(this.password_textBox);
+            this.panel1.Controls.Add(this.port_textBox);
+            this.panel1.Controls.Add(this.ipAddress_textBox);
+            this.panel1.Controls.Add(this.username_textBox);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1184, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // connect_button
+            // 
+            this.connect_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connect_button.Font = new System.Drawing.Font("Constantia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect_button.Location = new System.Drawing.Point(1035, 6);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(137, 34);
+            this.connect_button.TabIndex = 8;
+            this.connect_button.Text = "Connect";
+            this.connect_button.UseVisualStyleBackColor = true;
+            // 
+            // password_textBox
+            // 
+            this.password_textBox.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_textBox.Location = new System.Drawing.Point(762, 10);
+            this.password_textBox.Name = "password_textBox";
+            this.password_textBox.Size = new System.Drawing.Size(143, 27);
+            this.password_textBox.TabIndex = 7;
+            this.password_textBox.UseSystemPasswordChar = true;
+            // 
+            // port_textBox
+            // 
+            this.port_textBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.port_textBox.Location = new System.Drawing.Point(578, 10);
+            this.port_textBox.Name = "port_textBox";
+            this.port_textBox.Size = new System.Drawing.Size(54, 26);
+            this.port_textBox.TabIndex = 6;
+            // 
+            // ipAddress_textBox
+            // 
+            this.ipAddress_textBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipAddress_textBox.Location = new System.Drawing.Point(353, 10);
+            this.ipAddress_textBox.Name = "ipAddress_textBox";
+            this.ipAddress_textBox.Size = new System.Drawing.Size(143, 26);
+            this.ipAddress_textBox.TabIndex = 5;
+            // 
+            // username_textBox
+            // 
+            this.username_textBox.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username_textBox.Location = new System.Drawing.Point(101, 10);
+            this.username_textBox.Name = "username_textBox";
+            this.username_textBox.Size = new System.Drawing.Size(143, 27);
+            this.username_textBox.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(679, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(534, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(261, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ip Address:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.Override_button);
+            this.panel2.Controls.Add(this.SaveLib_button);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 658);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(400, 424);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(784, 284);
+            this.panel3.TabIndex = 2;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(400, 50);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(784, 374);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 20;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AndroidAutoApp",
+            "libAndroidAutoAppLogic.so",
+            "libAndroidAutoAppFramework.so",
+            "libAAutoHMILib.so"});
+            this.comboBox1.Location = new System.Drawing.Point(71, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(264, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // SaveLib_button
+            // 
+            this.SaveLib_button.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveLib_button.Location = new System.Drawing.Point(71, 191);
+            this.SaveLib_button.Name = "SaveLib_button";
+            this.SaveLib_button.Size = new System.Drawing.Size(264, 52);
+            this.SaveLib_button.TabIndex = 1;
+            this.SaveLib_button.Text = "Save lib from target";
+            this.SaveLib_button.UseVisualStyleBackColor = true;
+            // 
+            // Override_button
+            // 
+            this.Override_button.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Override_button.Location = new System.Drawing.Point(71, 294);
+            this.Override_button.Name = "Override_button";
+            this.Override_button.Size = new System.Drawing.Size(264, 52);
+            this.Override_button.TabIndex = 2;
+            this.Override_button.Text = "Override lib";
+            this.Override_button.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Constantia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(66, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(194, 26);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Select library type:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 708);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "LibOverride";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox password_textBox;
+        private System.Windows.Forms.TextBox port_textBox;
+        private System.Windows.Forms.TextBox ipAddress_textBox;
+        private System.Windows.Forms.TextBox username_textBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Override_button;
+        private System.Windows.Forms.Button SaveLib_button;
+    }
+}
+
