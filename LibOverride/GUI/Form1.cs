e5caca9959a44b0fe49c86875e2a0bb85cb77600
Septimiu Bar <bar.septimiu@gmail.com>
2020-05-07 16:44:12 +0300

    Load cached values for log in

--- LibOverride/GUI/Form1.cs
+++ LibOverride/GUI/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +20,40 @@ namespace GUI
         {
             InitializeComponent();
             webBrowserViewModel = new WebBrowserViewModel(webBrowser1);
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\CachedValues.txt");
+            LoadCachedValues(filePath);
+        }
+
+        private void LoadCachedValues(string filePath)
+        {
+            List<String> values = new List<string>();
+
+            try
+            {
+                using (StreamReader fileReader = File.OpenText(filePath))
+                {
+                    while (!fileReader.EndOfStream)
+                    {
+                        values.Add(fileReader.ReadLine());
+                    }
+                }
+
+                username_textBox.Text = values[0];
+                ipAddress_textBox.Text = values[1];
+                port_textBox.Text = values[2];
+                if (values.Count > 3)
+                    password_textBox.Text = values[3];
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            
+
             comboBox1.SelectedIndex = comboBox1.FindStringExact("libAndroidAutoAppLogic.so");
+
+            // change the focus from textboxes
+            webBrowser1.TabIndex = 0;
         }
 
         private void goBack_button_Click(object sender, EventArgs e)
